4b69d29bee8b1099dd6108d9706a877d12315e60
AnsloneKritisch <digveshoptimus@gmail.com>
2022-09-24 15:40:58 +0530

    new

---
+++ JavaScript/01_introduction/intro.js
@@ -0,0 +1,5 @@
+alert("Welome") ;
+
+var deposit = prompt("How much would you like to deposit today ? ");
+alert("You have Deposited: "+deposit);
+console.log("You are a cool Person !");
---
+++ JavaScript/Project/p1/weight.js
@@ -0,0 +1,5 @@
+var kg = prompt("Weight in kg ?") 
+var g = kg * 1000 ;
+
+alert("Your weight "+kg+"in grams "+g);
+console.log("Conversion Completed !");
